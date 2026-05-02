679ee0c727d5331863fb8a015a6cf3e72d5e395c
ArkoprobhaMazumder <arkoprobha.majumder1999@gmail.com>
2026-05-02 20:04:13 +0530

    Navbar Refactor Start

--- src/Components/navbar/Navbar.jsx
+++ src/Components/navbar/Navbar.jsx
@@ -9,63 +9,73 @@ const Navbar = () => {
   const [showMobileNav, setShowMobileNav] = useState(false);
   return (
     <>
-      <div className={css.navbar}>
-        <div className={css.navMain}>
-          <div className={css.navLogoBox}>
-            <NavLink to='/'>
-              <img src={assets.logo} alt="FOREVER_LOGO" loading='lazy' />
-            </NavLink>
-          </div>
+      <div className="container border-bottom">
 
-          <ul className={css.navLinkBox}>
-            <NavLink to="/" className={(({ isActive }) => isActive ? css.navLinkActive : css.navLink)}>
-              HOME
-            </NavLink>
-            <NavLink to="/collection" className={(({ isActive }) => isActive ? css.navLinkActive : css.navLink)}>
-              COLLECTION
-            </NavLink>
-            <NavLink to="/about" className={(({ isActive }) => isActive ? css.navLinkActive : css.navLink)}>
-              ABOUT
-            </NavLink>
-            <NavLink to="/contact" className={(({ isActive }) => isActive ? css.navLinkActive : css.navLink)}>
-              CONTACT
-            </NavLink>
-          </ul>
+        <div className={css.navbar}>
+          <div className={css.navMain}>
+            <div className={css.navLogoBox}>
+              <NavLink to='/'>
+                <img src={assets.logo} alt="FOREVER_LOGO" loading='lazy' />
+              </NavLink>
+            </div>
 
-          <div className={css.navUtilityBox}>
-            <div className={css.navUserProfile}>
-              <div className={css.searchLogoBox}>
-                <i className="fa-solid fa-magnifying-glass"></i>
-              </div>
-              <div className={css.userLogoBox}>
-                <i className="fa-solid fa-circle-user"></i>
-                <div className={css.userProfileContainer}>
-                  <NavLink to='/about' className={({ isActive }) => isActive ? 'fw-bold fs-5' : 'fw-bold fs-5'}>
-                    Profile
-                  </NavLink>
-                  <NavLink to='/order' className={({ isActive }) => isActive ? 'fw-bold fs-5' : 'fw-bold fs-5'}>
-                    Orders
-                  </NavLink>
-                  <NavLink to='/login' className={({ isActive }) => isActive ? 'fw-bold fs-5' : 'fw-bold fs-5'}>
-                    Logout
-                  </NavLink>
+            <ul className={css.navLinkBox}>
+              <NavLink to="/" className={(({ isActive }) => isActive ? css.navLinkActive : css.navLink)}>
+                HOME
+              </NavLink>
+              <NavLink to="/collection" className={(({ isActive }) => isActive ? css.navLinkActive : css.navLink)}>
+                COLLECTION
+              </NavLink>
+              <NavLink to="/about" className={(({ isActive }) => isActive ? css.navLinkActive : css.navLink)}>
+                ABOUT
+              </NavLink>
+              <NavLink to="/contact" className={(({ isActive }) => isActive ? css.navLinkActive : css.navLink)}>
+                CONTACT
+              </NavLink>
+            </ul>
+
+            <div className={css.navUtilityBox}>
+              <div className={css.navUserProfile}>
+                <div className={css.searchLogoBox}>
+                  {/* <i className="fa-solid fa-magnifying-glass"></i> */}
+                  <img src={assets.search_icon} alt="forevr_search" loading='lazy' />
+                </div>
+                <div className={css.userLogoBox}>
+                  {/* <i className="fa-solid fa-circle-user"></i> */}
+                  <img src={assets.profile_icon} alt="forevr_user" loading='lazy' />
+                  <div className={css.userProfileContainer}>
+                    <NavLink to='/about' className={({ isActive }) => isActive ? 'fw-bold fs-5' : 'fw-bold fs-5'}>
+                      Profile
+                    </NavLink>
+                    <NavLink to='/order' className={({ isActive }) => isActive ? 'fw-bold fs-5' : 'fw-bold fs-5'}>
+                      Orders
+                    </NavLink>
+                    <NavLink to='/login' className={({ isActive }) => isActive ? 'fw-bold fs-5' : 'fw-bold fs-5'}>
+                      Logout
+                    </NavLink>
+                  </div>
+                </div>
+                <div className={css.cartLogoBox}>
+                  <img src={assets.cart_icon} alt="forevr_cart" loading='lazy' />
+                  <div className={css.cartItemCount}>0</div>
                 </div>
               </div>
             </div>
-          </div>
 
-          {/* Mobile hamburger menu Responsive */}
-          <div className={css.navBurger} onClick={()=>setShowMobileNav(true)}>
-            <span className="material-symbols-outlined">menu</span>
+            {/* Mobile hamburger menu Responsive */}
+            <div className={css.navBurger} onClick={() => setShowMobileNav(true)}>
+              {/* <span className="material-symbols-outlined">menu</span> */}
+              <img src={assets.menu_icon} alt="nav_burger" loading='lazy'/>
+            </div>
+            <NavbarMobile
+              visible={showMobileNav}
+              setShowMobileNav={setShowMobileNav}
+            />
           </div>
-          <NavbarMobile 
-            visible={showMobileNav} 
-            setShowMobileNav={setShowMobileNav}
-          />
         </div>
       </div>
       <Outlet />
-      <Footer/>
+      <Footer />
     </>
   )
 }
